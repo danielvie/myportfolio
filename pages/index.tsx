24136ebc7b3dc3d2580541e5a4cbc4a6cc09c8be
Daniel Vieira <danielvie@gmail.com>
2023-07-28 21:53:21 +0200

    move to pages folder

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import 'app/globals.css'
+
+function Index() {
+    return (
+        <>
+            <div className="text-sky-500">
+            Hello
+            </div>
+        </>
+    )
+}
+
+export default Index
