6dfc2cdcc2805e8b6f336eedcafc45f23aab31c2
Daniel Vieira <danielvie@gmail.com>
2023-07-28 23:17:07 +0200

    update

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,30 +1,25 @@
 function Header() {
     return (
         <>
-            <div className="flex w-full h-12 bg-gray-700 justify-center items-center uppercase">
-                <div className="flex-1 text-white px-4 bg-sky-600 sm:py-3"></div>
-                <div className="flex-auto h-full text-center hover:bg-blue-500">
+            <div className="flex h-12 w-full items-center justify-center bg-gray-700 uppercase">
+                <div className="flex-1 bg-sky-600 px-4 text-white sm:py-3"></div>
+                <div className="h-full flex-auto text-center hover:bg-blue-500">
                     home
                 </div>
-                <div className="flex-auto h-full text-center w-2 hover:bg-blue-500">
+                <div className="h-full w-2 flex-auto text-center hover:bg-blue-500">
                     about
                 </div>
-                <div className="flex-auto h-full text-center hover:bg-blue-500">
+                <div className="h-full flex-auto text-center hover:bg-blue-500">
                     resume
                 </div>
-                <div className="flex-auto h-full text-center hover:bg-blue-500">
+                <div className="h-full flex-auto text-center hover:bg-blue-500">
                     work
                 </div>
-                <div className="flex-auto h-full text-center hover:bg-blue-500">
+                <div className="h-full flex-auto text-center hover:bg-blue-500">
                     contact
                 </div>
                 <div className="flex-1"></div>
             </div>
-
-            <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
-                blaa
-            </button>
-            <div className="p-4 text-lg bg-red-500 flex">Content here</div>
         </>
     );
 }
