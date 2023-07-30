0b6cd5af623b7fdb1b399bf219bb755144108ca4
Daniel Vieira <danielvie@gmail.com>
2023-07-30 21:35:52 +0200

    add head title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import Footer from "@/app/components/footer";
 import Header from "@/app/components/header";
 import Main from "@/app/components/main";
 import "app/globals.css";
+import Head from "next/head";
 
 function Index() {
     return (
         <>
+            <Head>
+                <title>Daniel Vieira</title>
+            </Head>
+
             <div className="min-h-screen min-w-[600px] font-mono text-white">
                 <Header></Header>
                 <Main></Main>
