5939d563c1b02a1e487b6c6b9efd262b8aa8d668
Daniel Vieira <danielvie@gmail.com>
2023-08-09 23:08:59 +0200

    add handleSend

--- app/components/contactme.tsx
+++ app/components/contactme.tsx
@@ -1,13 +1,15 @@
+import axios from "axios";
+
 function ContactMe() {
-    function handleSend() {
+    async function handleSend() {
         const input_name = (
             document.getElementById("contact_input_name") as HTMLInputElement
         ).value;
         const input_email = (
             document.getElementById("contact_input_email") as HTMLInputElement
         ).value;
-        const input_title = (
-            document.getElementById("contact_input_title") as HTMLInputElement
+        const input_subject = (
+            document.getElementById("contact_input_subject") as HTMLInputElement
         ).value;
         const input_message = (
             document.getElementById(
@@ -15,18 +17,42 @@ function ContactMe() {
             ) as HTMLTextAreaElement
         ).value;
 
-        console.log("input_name:", input_name);
-        console.log("input_email:", input_email);
-        console.log("input_title:", input_title);
-        console.log("input_message:", input_message);
+        let msg = `\`\`\`\n`;
+        msg += `name ....: ${input_name}\n`;
+        msg += `email ...: ${input_email}\n`;
+        msg += `subject .: ${input_subject}\n`;
+        msg += `\`\`\`\n`;
+        msg += `${input_message}`;
+        // email: ${input_email}
+        // ${input_subject}
+
+        // ${input_message}`
+
+        try {
+            const url: string =
+                process.env.NEXT_PUBLIC_DISCORD_WEBHOOK_URL || "";
+
+            const data = {
+                content: msg,
+                username: "mado portfolio",
+            };
+
+            const result = await axios.post(url!, data);
+
+            if (result.status === 204) {
+                console.log("message sent succesffully");
+            }
+        } catch (error) {
+            console.error("failed to send message: ", error);
+        }
     }
 
     return (
         <>
             <div className="m-auto w-3/4 items-center justify-center">
                 <div className="mb-3 flex items-center justify-center">
-                    <div className="w-[70px] text-right">Name:</div>
-                    <div className="ml-4 flex-auto">
+                    <div className="w-[85px] text-right">Name:</div>
+                    <div className="ml-2 flex-auto">
                         <input
                             id="contact_input_name"
                             className="w-full rounded-md bg-gray-300 p-3 text-gray-800 focus:bg-gray-100"
@@ -35,8 +61,8 @@ function ContactMe() {
                     </div>
                 </div>
                 <div className="mb-3 flex items-center justify-center">
-                    <div className="w-[70px] text-right">Email:</div>
-                    <div className="ml-4 flex-auto">
+                    <div className="w-[85px] text-right">Email:</div>
+                    <div className="ml-2 flex-auto">
                         <input
                             id="contact_input_email"
                             className="w-full rounded-md bg-gray-300 p-3 text-gray-800 focus:bg-gray-100"
@@ -45,18 +71,18 @@ function ContactMe() {
                     </div>
                 </div>
                 <div className="mb-3 flex items-center justify-center">
-                    <div className="w-[70px] text-right">Title:</div>
-                    <div className="ml-4 flex-auto">
+                    <div className="w-[85px] text-right">Subject:</div>
+                    <div className="ml-2 flex-auto">
                         <input
-                            id="contact_input_title"
+                            id="contact_input_subject"
                             className="w-full rounded-md bg-gray-300 p-3 text-gray-800 focus:bg-gray-100"
                             type="text"
                         />
                     </div>
                 </div>
                 <div className="mb-3 flex items-start justify-center">
-                    <div className="w-[70px] text-right">Message:</div>
-                    <div className="ml-4 flex-auto">
+                    <div className="w-[85px] text-right">Message:</div>
+                    <div className="ml-2 flex-auto">
                         <textarea
                             id="contact_input_message"
                             className="w-full rounded-md bg-gray-300 p-3 text-gray-800 focus:bg-gray-100"
@@ -68,7 +94,7 @@ function ContactMe() {
                     onClick={handleSend}
                     className="rounded-md bg-sky-800 px-4 py-2 hover:bg-sky-900"
                 >
-                    SEND
+                    SEND MESSAGE
                 </button>
             </div>
         </>
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,21 +1,6 @@
 import Nav from "./nav";
 
 function Header() {
-    function handleScroolToElement(id: string) {
-        const el = document.getElementById(id);
-
-        if (el) {
-            const headerHeight = 60;
-            const elementPostition = el.getBoundingClientRect().top;
-            const offsetPostion = elementPostition - headerHeight;
-
-            window.scrollBy({
-                top: offsetPostion,
-                behavior: "smooth",
-            });
-        }
-    }
-
     return (
         <>
             <Nav />
