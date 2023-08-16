5a970e261cc7dffaac4002fcec52f8cdde296b5b
Daniel Vieira <danielvie@gmail.com>
2023-08-17 00:47:27 +0200

    add check for form valid

--- app/components/contactme.tsx
+++ app/components/contactme.tsx
@@ -5,35 +5,75 @@ enum STATE_MSG {
     IDLE = 0,
     SENDING,
     SENT,
+    ERROR,
 }
 
 function ContactMe() {
     const [msgSent, setMsgSent] = useState(STATE_MSG.IDLE);
 
+    function checkInput(
+        element: HTMLInputElement | HTMLTextAreaElement,
+        msg: string
+    ) {
+        if (element.value.trim() === "") {
+            element.setAttribute("placeholder", msg);
+            element?.classList.add("border-2", "border-red-700");
+
+            return false;
+        }
+        element?.classList.remove("border-2");
+        return true;
+    }
+
     async function handleSend() {
         console.log("Begin:handleSend");
-        const input_name = (
-            document.getElementById("contact_input_name") as HTMLInputElement
-        ).value;
-        const input_email = (
-            document.getElementById("contact_input_email") as HTMLInputElement
-        ).value;
-        const input_subject = (
-            document.getElementById("contact_input_subject") as HTMLInputElement
-        ).value;
-        const input_message = (
-            document.getElementById(
-                "contact_input_message"
-            ) as HTMLTextAreaElement
-        ).value;
 
+        const i_name = document.getElementById(
+            "contact_input_name"
+        ) as HTMLInputElement;
+        const name = i_name.value;
+
+        const i_email = document.getElementById(
+            "contact_input_email"
+        ) as HTMLInputElement;
+        const email = i_email.value;
+
+        const i_subject = document.getElementById(
+            "contact_input_subject"
+        ) as HTMLInputElement;
+        const subject = i_subject.value;
+
+        const i_message = document.getElementById(
+            "contact_input_message"
+        ) as HTMLTextAreaElement;
+        const message = i_message.value;
+
+        // check if name is filled
+        const name_isvalid = checkInput(i_name, "name cannot be empty!");
+        const email_isvalid = checkInput(i_email, "email cannot be empty!");
+        const message_isvalid = checkInput(
+            i_message,
+            "message cannot be empty!"
+        );
+
+        const isValid = name_isvalid && email_isvalid && message_isvalid;
+
+        if (!isValid) {
+            setMsgSent(STATE_MSG.ERROR);
+            setTimeout(() => {
+                setMsgSent(STATE_MSG.IDLE);
+            }, 4000);
+            return false;
+        }
+
+        // create message to send
         let msg = "_ _";
         msg += "```\n";
-        msg += `name ....: ${input_name}\n`;
-        msg += `email ...: ${input_email}\n`;
-        msg += `subject .: ${input_subject}\n`;
+        msg += `name ....: ${name}\n`;
+        msg += `email ...: ${email}\n`;
+        msg += `subject .: ${subject}\n`;
         msg += "```\n";
-        msg += input_message;
+        msg += message;
         msg += "_ _\n";
         msg += "_ _\n";
         msg += "```";
@@ -62,7 +102,9 @@ function ContactMe() {
         <>
             <div className="m-auto mt-8 items-center justify-center md:w-5/6">
                 <div className="mb-3 flex items-center justify-center">
-                    <div className="w-[10ex] text-right">Name:</div>
+                    <div className="w-[10ex] text-right">
+                        Name<sup className="text-sky-300">*</sup>:
+                    </div>
                     <div className="ml-2 flex-auto">
                         <input
                             id="contact_input_name"
@@ -72,7 +114,9 @@ function ContactMe() {
                     </div>
                 </div>
                 <div className="mb-3 flex items-center justify-center">
-                    <div className="w-[10ex] text-right">Email:</div>
+                    <div className="w-[10ex] text-right">
+                        Email<sup className="text-sky-300">*</sup>:
+                    </div>
                     <div className="ml-2 flex-auto">
                         <input
                             id="contact_input_email"
@@ -92,7 +136,9 @@ function ContactMe() {
                     </div>
                 </div>
                 <div className="mb-3 flex items-start justify-center">
-                    <div className="w-[10ex] text-right">Message:</div>
+                    <div className="w-[10ex] text-right">
+                        Message<sup className="text-sky-300">*</sup>:
+                    </div>
                     <div className="ml-2 flex-auto">
                         <textarea
                             id="contact_input_message"
@@ -103,11 +149,15 @@ function ContactMe() {
                 </div>
                 {msgSent == STATE_MSG.SENDING ? (
                     <button className="rounded-md bg-orange-800 px-4 py-2">
-                        <label className="animate-pulse">SENDING...</label>
+                        <span className="animate-pulse">SENDING...</span>
                     </button>
                 ) : msgSent == STATE_MSG.SENT ? (
                     <button className="rounded-md bg-green-800 px-4 py-2">
                         MESSAGE SENT!
+                    </button>
+                ) : msgSent == STATE_MSG.ERROR ? (
+                    <button className="rounded-md bg-red-800 px-4 py-2">
+                        <span className="animate-pulse">NOT VALID!!</span>
                     </button>
                 ) : (
                     <button
